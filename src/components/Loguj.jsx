e67ce315fbf15902829e10dbcfc8ccb3e97c2b3d
Damian <swean@vp.pl>
2023-10-27 00:27:57 +0200

    Update Loguj.jsx

--- src/components/Loguj.jsx
+++ src/components/Loguj.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
 export default function Loguj(){
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })

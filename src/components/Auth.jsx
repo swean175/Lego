e1b550908312b21a9d83831b99316465f46b6d1f
Damian <swean@vp.pl>
2023-10-27 22:58:11 +0200

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,41 +4,40 @@ import { Outlet, Navigate, useLocation } from "react-router-dom"
 export default function Auth() {
   let isLoggedIn = false
    let tok = sessionStorage.getItem("logData")
-   
-   checkAuthentication()
 
 
+React.useEffect(()=>{
+  async function token(message){
+    const serUrl = 'https://slawa-lego-team.netlify.app/.netlify/functions/token'
+    const response = await fetch(serUrl, {
+ method: 'POST',
+ headers: {
+     'Content-Type': 'application/json'
+ },
+ body:JSON.stringify(message)
+   })
+ 
+ const data = await response.json()
+   return data
+ }
 
-    async function token(message){
-        const serUrl = 'https://slawa-lego-team.netlify.app/.netlify/functions/token'
-        const response = await fetch(serUrl, {
-     method: 'POST',
-     headers: {
-         'Content-Type': 'application/json'
-     },
-     body:JSON.stringify(message)
-       })
-     
-     const data = await response.json()
-       return data
-     }
-
-     async function checkAuthentication() {
-        try {
-          const value = await token(tok)
-          console.log("value " + value)
-          isLoggedIn = await value
-          console.log("Authenticated " + value)
-        } catch (error) {
-          console.error("error with Authentification " + value)
-        }
-     
-      }
+ async function checkAuthentication() {
+    try {
+      const value = await token(tok)
+      console.log("value " + value)
+      isLoggedIn = await value
+      console.log("Authenticated " + value)
+    } catch (error) {
+      console.error("error with Authentification " + value)
+    }
+ 
+  }
+  checkAuthentication()
+},[])
 
 
     const location = useLocation()
 
- 
 
     console.log("isLogged "+ isLoggedIn)
     if (isLoggedIn === false) {

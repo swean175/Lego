646a65ee10479af9e6d52623db11f48e0e10a968
Damian <swean@vp.pl>
2023-10-28 00:01:02 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 import React from "react"
+import { Navigate } from "react-router-dom"
 import { LuUserCircle2 } from "react-icons/lu"
 
 export default function Footer(){
 const [user, setUser] = React.useState("")
 const [wyloguj, setWyloguj] = React.useState("")
+const [logOut, setLogOut] = React.useState(false)
 const name = sessionStorage.getItem("Name")
 
 function logOff(){
     sessionStorage.removeItem("Name")
     sessionStorage.removeItem("logData")
+    setLogOut(true)
 }
 
 React.useEffect(() => {
@@ -18,6 +21,7 @@ React.useEffect(() => {
     }
   }, [name]);
 
+  if(!logOut){
     return(
         <>
         <footer> 
@@ -27,4 +31,15 @@ React.useEffect(() => {
        
         </>
     )
+  }
+  return (
+    <Navigate 
+    to="/" 
+    state={{
+        message: "Wylogowano",
+        from: location.pathname
+    }} 
+    replace
+/>
+  )
 }

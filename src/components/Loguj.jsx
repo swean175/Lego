a3fd620a2731e030e0470b331c0062dfc5f19aa7
Damian Szudra <123982916+swean175@users.noreply.github.com>
2023-11-04 20:28:41 +0100

    Update Loguj.jsx

--- src/components/Loguj.jsx
+++ src/components/Loguj.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import { Navigate } from "react-router-dom"
 
 export default function Loguj(){
+    const admin = JSON.parse(process.env.CREDS_ADMIN)
+    console.log(admin)
 
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const [aproved, setAproved] = React.useState(false)

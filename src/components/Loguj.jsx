2779f4ca4f6cceea7ebcf722ccbfb3b692103405
Damian Szudra <123982916+swean175@users.noreply.github.com>
2023-11-04 20:44:47 +0100

    Update Loguj.jsx

--- src/components/Loguj.jsx
+++ src/components/Loguj.jsx
@@ -28,7 +28,7 @@ export default function Loguj(){
         e.preventDefault()
            try{
             const token = await Log(loginFormData);
-            saveToken(token);
+            token !== "" ? saveToken(token):null
             setAproved(() => token.aprove)
             !aproved? setKomunikat("Adres email lub hasło jest nieprawidłowe") : null
            }catch(error){

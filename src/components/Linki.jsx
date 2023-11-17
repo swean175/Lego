313695e8b59cac1cf20968ccd624d74f834c5c8d
Damian <swean@vp.pl>
2023-11-17 19:14:52 +0100

    Update Linki.jsx

--- src/components/Linki.jsx
+++ src/components/Linki.jsx
@@ -11,8 +11,10 @@ export default function Linki(){
             <h3><a href="https://makecode.mindstorms.com" target="blank">Online edytor Makecode</a></h3>
         <hr></hr>
         <h3><a href=" https://education.lego.com/pl-pl/downloads/mindstorms-ev3/software/" target="blank">Program Ev3 Clasroom<img className="ev3logo" src={classroom} alt="Link do programu Ev3 Clasroom"/> </a></h3>
+        <br></br>
         <hr></hr>
         <h3><a href=" https://education.lego.com/pl-pl/downloads/retiredproducts/mindstorms-ev3-lab/software/" target="blank">Lego mindstorms Education Ev3 lub Ev3 Labs<img className="ev3logo" src={ev} alt="Link do programu Lego mindstorms Education Ev3 lub Ev3 Labs"/> </a></h3>
+        <br></br>
         <hr></hr>
         <h3><a href="https://ev3-help-online.api.education.lego.com/Education/en-gb/page.html?Path=editor%2FProgrammingEnvironment.html" target="blank">Dokumentacja LegoEducation Ev3</a></h3>
        <hr></hr>

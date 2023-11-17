dd0606abadf52b7c2f3db682eaba690873ab3e60
Damian <swean@vp.pl>
2023-11-17 18:54:05 +0100

    Update Linki.jsx

--- src/components/Linki.jsx
+++ src/components/Linki.jsx
@@ -8,16 +8,15 @@ export default function Linki(){
             <div className="przydatne-linki">
             <h1>Przydatne Linki</h1>
             <hr></hr>
-       
-        <h3><a href="https://www.youtube.com/watch?v=7-4yOx1CnXE" target="blank">Fabryka Tesla</a></h3>
+            <h3><a href="https://makecode.mindstorms.com" target="blank">Online edytor Makecode</a></h3>
+        <hr></hr>
+        <h3><a href=" https://education.lego.com/pl-pl/downloads/mindstorms-ev3/software/" target="blank">Program Ev3 Clasroom<img className="ev3logo" src={classroom} alt="Link do programu Ev3 Clasroom"/> </a></h3>
+        <hr></hr>
+        <h3><a href=" https://education.lego.com/pl-pl/downloads/retiredproducts/mindstorms-ev3-lab/software/" target="blank">Lego mindstorms Education Ev3 lub Ev3 Labs<img className="ev3logo" src={ev} alt="Link do programu Lego mindstorms Education Ev3 lub Ev3 Labs"/> </a></h3>
         <hr></hr>
         <h3><a href="https://ev3-help-online.api.education.lego.com/Education/en-gb/page.html?Path=editor%2FProgrammingEnvironment.html" target="blank">Dokumentacja LegoEducation Ev3</a></h3>
-        <hr></hr>
-        <h3><a href="https://makecode.mindstorms.com" target="blank">Online edytor Makecode</a></h3>
-        <hr></hr>
-       <h3><a href=" https://education.lego.com/pl-pl/downloads/mindstorms-ev3/software/" target="blank">Program Ev3 Clasroom<img className="ev3logo" src={classroom} alt="Link do programu Ev3 Clasroom"/> </a></h3>
        <hr></hr>
-       <h3><a href=" https://education.lego.com/pl-pl/downloads/retiredproducts/mindstorms-ev3-lab/software/" target="blank">Lego mindstorms Education Ev3 lub Ev3 Labs<img className="ev3logo" src={ev} alt="Link do programu Lego mindstorms Education Ev3 lub Ev3 Labs"/> </a></h3>
+       <h3><a href="https://www.youtube.com/watch?v=7-4yOx1CnXE" target="blank">Fabryka Tesla</a></h3>
        <hr></hr>
        <h3><a href="https://www.youtube.com/watch?v=3tguWcKTXQI" target="blank">Zawody Sumo </a></h3>
  

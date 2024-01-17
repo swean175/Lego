515431cb6318a2a20472c6e192bac278e22a62e1
Damian <swean@vp.pl>
2024-01-17 19:51:31 +0100

    s

--- src/components/Lekcje.jsx
+++ src/components/Lekcje.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 export default function Lekcje(){
 const [disp, setDisp] = React.useState("Wprowadzenie")
-    const tematyArr = ["Wprowadzenie", "lekcja1", "lekcja2", "lekcja3", "lekcja4", "lekcja5", "lekcja6"]
+    const tematyArr = ["Wprowadzenie", "lekcja1", "lekcja2", "lekcja3", "lekcja4", "lekcja5"]
 
 
     const activeStyles = {

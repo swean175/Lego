67006dcd5a635a9e7ecace1f310ba2cf302bac89
Damian <swean@vp.pl>
2024-01-18 18:09:35 +0100

    e

--- src/components/svgs/droga.jsx
+++ src/components/svgs/droga.jsx
@@ -2,7 +2,7 @@ export default function Droga(){
 
     return(
       
-   <svg id="droga" height="10" width="650" xmlns="http://www.w3.org/2000/svg">
+   <svg id="droga" height="10" width="350" xmlns="http://www.w3.org/2000/svg">
         <line x1="0" y1="5" x2="650" y2="5" stroke="red" />
         </svg>
      
--- src/components/svgs/odc.jsx
+++ src/components/svgs/odc.jsx
@@ -2,7 +2,7 @@ export default function Odc({id, x2}){
 
 
     return(
-    <svg class="odcdrogi" id={id} height="10" width="200" xmlns="http://www.w3.org/2000/svg">
+    <svg class="odcdrogi" id={id} height="10" width="100" xmlns="http://www.w3.org/2000/svg">
         <line x1="0" y1="5" x2={x2} y2="5" stroke="blue" />
         </svg>
         

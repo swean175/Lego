df367ca713ac406b43fca2c7ca39fc8121186ce4
Damian Szudra <123982916+swean175@users.noreply.github.com>
2024-01-19 00:33:11 +0100

    Update droga.jsx

--- src/components/svgs/droga.jsx
+++ src/components/svgs/droga.jsx
@@ -2,8 +2,8 @@ export default function Droga(){
 
     return(
       
-   <svg id="droga" height="10" width="350" xmlns="http://www.w3.org/2000/svg">
-        <line x1="0" y1="5" x2="650" y2="5" stroke="red" />
+   <svg id="droga" height="10" width="243.7" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0" y1="5" x2="243.7" y2="5" stroke="red" />
         </svg>
      
     )}

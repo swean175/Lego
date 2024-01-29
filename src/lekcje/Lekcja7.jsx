4eee8104357fbf6feda21881a9e9107f3a7d5753
Damian <swean@vp.pl>
2024-01-29 19:56:13 +0100

    Update Lekcja7.jsx

--- src/lekcje/Lekcja7.jsx
+++ src/lekcje/Lekcja7.jsx
@@ -14,6 +14,9 @@ export default function Lekcja7(){
 const [deg, setDeg] = React.useState(stopnie.defaultValue)
 const [degB, setDegB] = React.useState(stopnieB.defaultValue)
 
+const [code, setCode] = React.useState(` <Robo className="robo"/>`)
+const [codeB, setCodeB] = React.useState(`  <Robo className="roboB"/>`)
+
       const centrum = document.getElementById('centrum')
 const slider = document.getElementById('slider')
 const stopnie = document.getElementById('stopnie')
@@ -26,24 +29,23 @@ const obrot = document.getElementById('obrot')
 
 
 let arr = []
-let code = '<img src="robo.svg" className="robo">'
-
 let arrB = []
-let codeB = ''
+
+
 
 stopnie.textContent = "0 stopni"
 stopnieB.textContent = "0 stopni"
 
 
-sliderB.addEventListener("change", (event)=>{
+function handleSliderB(event){
     setDegB(event.target.value) 
-codeB =`<div id="point" class="point"></div>
-<div class="centrumB" id="centrumB;">
-<img src="robo.svg" class="roboB"> 
-</div> `
+setCodeB (`<div id="point" className="point"></div>
+<div className="centrumB" id="centrumB;">
+<Robo className="roboB"/> 
+</div> `)
 arrB = []
 renderB()
-})
+}
 
 function renderB(){
 
@@ -51,17 +53,17 @@ function renderB(){
     for (let i =0; i <= degB; i++){
 
         let styleB = `"transform:translate(calc(cos(${(10*i)+180}deg)*138px),calc(sin(${(10*i)+180}deg)*138px));"`
-        arrB.push(`<div class="sladB" style=${styleB}></div>`) 
+        arrB.push(`<div className="sladB" style=${styleB}></div>`) 
 
-        codeB =`<div id="point" class="point"></div>
-        <div class="centrumB" id="centrumB" style="transform: rotate(${i*10}deg)">
-        <img src="robo.svg" class="roboB"> 
-        </div> `
+        setCodeB (`<div id="point" className="point"></div>
+        <div className="centrumB" id="centrumB" style="transform: rotate(${i*10}deg)">
+        <Robo className="roboB"/> 
+        </div> `)
     }
 
 
     arrB.forEach((it)=>{
-        codeB += it
+        setCodeB(prev => prev + it)
     })
 
     stopnieB.textContent = `${degB * 10} stopni`
@@ -73,24 +75,24 @@ function renderB(){
 //--------------------------------------------------------------------------------------------
 
 
-slider.addEventListener("change", (event)=>{
+function handleSlider (event){
    setDeg(event.target.value)
-code =`<img src="robo.svg" class="robo" style="transform: rotate(${deg*10}deg); 
-transition: 0.2s;">`
+setCode (`<Robo className="robo" style="transform: rotate(${deg*10}deg); 
+transition: 0.2s;"/>`)
 arr = []
 render()
-})
+}
 
 function render(){
 
     for (let i =0; i <= deg; i++){
         let style = `"transform:translate(calc(cos(${(10*i)-90}deg)*var(--offset)),calc(sin(${(10*i)-90}deg)*var(--offset)));"`
         let style2 = `"transform:translate(calc(cos(${(10*i)+90}deg)*var(--offset)),calc(sin(${(10*i)+90}deg)*var(--offset)));"`
-        arr.push(`<div class="slad" style=${style}></div><div class="slad2" style=${style2}></div>`) 
+        arr.push(`<div className="slad" style=${style}></div><div className="slad2" style=${style2}></div>`) 
     }
     
     arr.forEach((it)=>{
-        code += it
+        setCode(prev => prev + it)
     })
 
     stopnie.textContent = `${deg * 10} stopni`
@@ -114,7 +116,7 @@ function render(){
             </div> 
             </div>
 
-             <input id="sliderB" className="slider" value="0" type="range" name="pointsB" min="0" max="36"></input>
+             <input id="sliderB" className="slider" value="0" type="range" name="pointsB" min="0" max="36" onChange={handleSliderB}></input>
             <p id="stopnieB"></p>
 
         </div>
@@ -126,7 +128,7 @@ function render(){
                 <Robo className="robo"/>
             </div>
 
-            <input id="slider" className="slider" value="0" type="range" name="points" min="0" max="36"></input>
+            <input id="slider" className="slider" value="0" type="range" name="points" min="0" max="36" onChange={handleSlider}></input>
             <p id="stopnie"></p>
         </div>
 

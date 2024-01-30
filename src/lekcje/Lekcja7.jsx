acc9e310c9890344b0bc1518663b9cc2ced495a6
Damian <swean@vp.pl>
2024-01-30 11:26:11 +0100

    Update Lekcja7.jsx

--- src/lekcje/Lekcja7.jsx
+++ src/lekcje/Lekcja7.jsx
@@ -3,14 +3,11 @@ import Robo from "../components/svgs/robo"
 import oponaszer from "../assets/lekcja6/tyre.jpg"
 
 
-
 export default function Lekcja7(){
 
     useEffect(() => {
         window.scrollTo(0, 100)
       }, [])
-
-
 
 const [deg, setDeg] = useState(0)
 const [degB, setDegB] = useState(0)
@@ -43,43 +40,43 @@ function Centrum(){
 }
 
 
-function handleSliderB(event){
-    setDegB(event.target.value) 
+// function handleSliderB(event){
+//     setDegB(event.target.value) 
 
-let arrB = []
+// let arrB = []
 
-for (let i =0; i <= degB; i++){
-const stylB  = `{transform:"translate(calc(cos(${(10*i)+180}deg)*138px),calc(sin(${(10*i)+180}deg)*138px))"}`
-    arrB.push(`<div className="sladB" style={${stylB}}></div>`) 
+// for (let i =0; i <= degB; i++){
+// const stylB  = `{transform:"translate(calc(cos(${(10*i)+180}deg)*138px),calc(sin(${(10*i)+180}deg)*138px))"}`
+//     arrB.push(`<div className="sladB" style={${stylB}}></div>`) 
 
-    codeB = `<div id="point" className="point"></div>
-    <div className="centrumB" id="centrumB" style={{transform:"rotate(${i*10}deg)"}}>
-    <Robo className="roboB"/> 
-    </div>`
+//     codeB = `<div id="point" className="point"></div>
+//     <div className="centrumB" id="centrumB" style={{transform:"rotate(${i*10}deg)"}}>
+//     <Robo className="roboB"/> 
+//     </div>`
 
- codeB = codeB + [...arrB]
-}
-}
+//  codeB = codeB + [...arrB]
+// }
+// }
 
 
 //--------------------------------------------------------------------------------------------
 
 
-function handleSlider(event){
-   setDeg(event.target.value)
- code = `<Robo className="robo" style={{transform: "rotate(${deg*10}deg)", 
-   transition:" 0.2s"}}/>`
+// function handleSlider(event){
+//    setDeg(event.target.value)
+//  code = `<Robo className="robo" style={{transform: "rotate(${deg*10}deg)", 
+//    transition:" 0.2s"}}/>`
 
-let arr = []
+// let arr = []
 
-for (let i =0; i <= deg; i++){
-   const styl = `{transform:"translate(calc(cos(${(10*i)-90}deg)*var(--offset)),calc(sin(${(10*i)-90}deg)*var(--offset)))"}`
-   const styl2 = `{transform:"translate(calc(cos(${(10*i)+90}deg)*var(--offset)),calc(sin(${(10*i)+90}deg)*var(--offset)))"}`
-    arr.push(`<div className="slad" style={${styl}}></div><div className="slad2" style={${styl2}}></div>`) 
-}
+// for (let i =0; i <= deg; i++){
+//    const styl = `{transform:"translate(calc(cos(${(10*i)-90}deg)*var(--offset)),calc(sin(${(10*i)-90}deg)*var(--offset)))"}`
+//    const styl2 = `{transform:"translate(calc(cos(${(10*i)+90}deg)*var(--offset)),calc(sin(${(10*i)+90}deg)*var(--offset)))"}`
+//     arr.push(`<div className="slad" style={${styl}}></div><div className="slad2" style={${styl2}}></div>`) 
+// }
 
-code = code + [...arr]
-}
+// code = code + [...arr]
+// }
 
     return(
         <React.Fragment>
@@ -91,7 +88,7 @@ code = code + [...arr]
          
          <Obrot />
 
-             <input id="sliderB" className="slider" value="0" type="range" name="pointsB" min="0" max="36" onChange={handleSliderB}></input>
+             <input id="sliderB" className="slider" value="0" type="range" name="pointsB" min="0" max="36" ></input>
             <p id="stopnieB">{degB * 10} stopni</p>
 
         </div>
@@ -100,7 +97,7 @@ code = code + [...arr]
         <div className="skret">
       <Centrum />
 
-            <input id="slider" className="slider" value="0" type="range" name="points" min="0" max="36" onChange={handleSlider}></input>
+            <input id="slider" className="slider" value="0" type="range" name="points" min="0" max="36" ></input>
             <p id="stopnie">{deg * 10} stopni</p>
         </div>
 

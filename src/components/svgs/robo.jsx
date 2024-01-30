0994299f26872645bed145cd039103b429a34d87
Damian <swean@vp.pl>
2024-01-30 14:35:52 +0100

    Update robo.jsx

--- src/components/svgs/robo.jsx
+++ src/components/svgs/robo.jsx
@@ -3,8 +3,7 @@ export default function Robo(){
     return(
         <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="250"
-      height="250"
+     
       fillRule="evenodd"
       clipRule="evenodd"
       imageRendering="optimizeQuality"
